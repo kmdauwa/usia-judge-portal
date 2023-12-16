9ecec4e1be5bccf37dcb08263cbdd72e23dc93d1
Shaz Momin <shaz.momin@utexas.edu>
2023-12-15 18:09:28 -0600

    Initial db & drizzle setup

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import type {Config} from 'drizzle-kit'
+import * as dotenv from 'dotenv'
+dotenv.config({path: '.env'})
+
+if (!process.env.DATABASE_URL) {
+  console.log('DATABASE_URL is not defined')
+}
+
+export default {
+    schema:'./src/lib/supabase/schema.ts',
+    out: './migrations',
+    driver: 'pg',
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL || '',
+    }
+} satisfies Config
