9ebc83dc3f802c38200549515de9f07d916a9f87
Shaz Momin <shaz.momin@utexas.edu>
2024-01-10 09:52:53 -0600

    judge onboarding with signup auth done

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,138 +1,150 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import * as z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { FormSchema } from '@/lib/types';
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormSchema } from "@/lib/types";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form';
-import Link from 'next/link';
-import Image from 'next/image';
-import Logo from '../../../../public/gef2024logo.png';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { actionLoginUser } from '@/lib/server-actions/auth-actions';
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormMessage,
+} from "@/components/ui/form";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../../../public/gef2024logo.png";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { actionLoginUser } from "@/lib/server-actions/auth-actions";
+import { useSearchParams } from "next/navigation";
 
 const LoginPage = () => {
-  const router = useRouter();
-  const [submitError, setSubmitError] = useState('');
+	const router = useRouter();
+	const [submitError, setSubmitError] = useState("");
+	const code = useSearchParams().get("code");
 
-  const form = useForm<z.infer<typeof FormSchema>>({
-    mode: 'onChange',
-    resolver: zodResolver(FormSchema),
-    defaultValues: { email: '', password: '' },
-  });
+	const form = useForm<z.infer<typeof FormSchema>>({
+		mode: "onChange",
+		resolver: zodResolver(FormSchema),
+		defaultValues: { email: "", password: "" },
+	});
 
-  const isLoading = form.formState.isSubmitting;
+	const isLoading = form.formState.isSubmitting;
 
-  const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
-    formData
-  ) => {
-    const { error } = await actionLoginUser(formData);
-    if (error) {
-      form.reset();
-      setSubmitError(error.message);
-    } else {
-      router.replace('/home');
-    }
-  };
+	const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
+		formData
+	) => {
+		const { error } = await actionLoginUser(formData);
+		if (error) {
+			form.reset();
+			setSubmitError(error.message);
+		} else {
+			router.replace("/home");
+		}
+	};
 
-  return (
-    <Form {...form}>
-      <form
-        onChange={() => {
-          if (submitError) setSubmitError('');
-        }}
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col"
-      >
-        <Link
-          href="/"
-          className="
+	return (
+		<Form {...form}>
+			<form
+				onChange={() => {
+					if (submitError) setSubmitError("");
+				}}
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col">
+				<Link
+					href="/login"
+					className="
           w-full
           flex
           flex-col
           justify-left
-          items-center"
-        >
-          <Image
-            src={Logo}
-            alt="cypress Logo"
-            width={150}
-            height={150}
-          />
-          <span
-            className="font-semibold
-          dark:text-white text-4xl mt-4 mx-2"
-          >
-            USIA Judge Portal
-          </span>
-        </Link>
-        <FormDescription className="text-foreground/60 text-center">
-          Judging Portal for Ismaili Arts Festival
-        </FormDescription>
-        <FormField
-          disabled={isLoading}
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  type="email"
-                  placeholder="Email"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          disabled={isLoading}
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  type="password"
-                  placeholder="Password"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {submitError && <FormMessage>{submitError}</FormMessage>}
-        <Button
-          type="submit"
-          className="w-full p-6"
-          size="lg"
-          disabled={isLoading}
-        >
-          {!isLoading ? 'Login' : 'Loading...'}
-        </Button>
-        <span className="text-center">
-          Dont have an account?{' '}
-          <Link
-            href="/signup"
-            className="text-slate-600 dark:text-slate-400 hover:underline"
-          >
-            Sign Up
-          </Link>
-        </span>
-      </form>
-    </Form>
-  );
+          items-center">
+					<Image
+						src={Logo}
+						alt="cypress Logo"
+						width={150}
+						height={150}
+					/>
+					<span
+						className="font-semibold
+          dark:text-white text-4xl mt-4 mx-2">
+						USIA Judge Portal
+					</span>
+				</Link>
+				<FormDescription className="text-foreground/60 text-center">
+					Judging Portal for Ismaili Arts Festival
+				</FormDescription>
+				<FormField
+					disabled={isLoading}
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormControl>
+								<Input
+									type="email"
+									placeholder="Email"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					disabled={isLoading}
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<FormItem>
+							<FormControl>
+								<Input
+									type="password"
+									placeholder="Password"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				{submitError && <FormMessage>{submitError}</FormMessage>}
+				<Button
+					type="submit"
+					className="w-full p-6"
+					size="lg"
+					disabled={isLoading}>
+					{!isLoading ? "Login" : "Loading..."}
+				</Button>
+				<span className="text-center">
+					Dont have an account?{" "}
+					<Link
+						href="/signup"
+						className="text-slate-600 dark:text-slate-400 hover:underline">
+						Sign Up
+					</Link>
+				</span>
+				<>
+					{code && (
+						<>
+							<Alert className="bg-green-100 text-green-600 p-5">
+								<AlertTitle>
+									Your account has been created & verified!
+								</AlertTitle>
+								<AlertDescription>
+									Please log in to continue.
+								</AlertDescription>
+							</Alert>
+						</>
+					)}
+				</>
+			</form>
+		</Form>
+	);
 };
 
 export default LoginPage;
--- src/lib/server-actions/auth-actions.ts
+++ src/lib/server-actions/auth-actions.ts
@@ -4,6 +4,15 @@ import { z } from 'zod';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { FormSchema } from '../types';
 import { cookies } from 'next/headers';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres'
+import {users, userType, judges} from '../supabase/schema'
+import { eq, lt, gte, ne, sql } from 'drizzle-orm';
+
+const connectionString = process.env.DATABASE_URL as string
+
+const client = postgres(connectionString)
+const db = drizzle(client);
 
 export async function actionLoginUser({
   email,
@@ -22,12 +31,21 @@ export async function actionSignUpUser({
   password,
 }: z.infer<typeof FormSchema>) {
   const supabase = createRouteHandlerClient({ cookies });
-  const { data } = await supabase
+  const { data: profileData } = await supabase
     .from('profiles')
     .select('*')
     .eq('email', email);
 
-  if (data?.length) return { error: { message: 'User already exists', data } };
+  if (profileData?.length) return { error: { message: 'User already exists', profileData } };
+
+  // Check if the email is part of the judges table
+  const judgesResult = await db.select({
+    email: judges.email
+  }).from(judges).where(eq(judges.email, email))
+  
+  if (judgesResult.length == 0) return { error: { message: 'This email is not authorized to judge', data: judgesResult } };
+
+  // Continue with the sign up process
   const response = await supabase.auth.signUp({
     email,
     password,
